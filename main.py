61bee6011a8be7b89df1bc777b2a88194f67ba1f
Luca Battesini <lucabattesini1@gmail.com>
2025-07-10 16:46:43 -0400

    Creating a function which will verify if the file inside db it's an csv

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
+
+file_path = "./db"
+file = os.listdir(file_path)
+
+if file[0].endswith(".csv"):
+    print("Verified")
+else: 
+    raise ValueError("It isn't a csv file")
+
+df = f"./db/{file[0]}"
