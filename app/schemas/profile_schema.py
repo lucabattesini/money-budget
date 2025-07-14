50ddc262be6f51f4f31cc19ca000aea57553675e
Luca Battesini <lucabattesini1@gmail.com>
2025-07-14 11:19:37 -0400

    Creating a simpler schema to create a profile

--- app/schemas/profile_schema.py
+++ app/schemas/profile_schema.py
@@ -5,3 +5,11 @@ class UserProfile(BaseModel):
     email: str
     password: str
     name: str
+
+class UserInvestmentsProfile(BaseModel):
+    fixed_expenses: list
+    fixed_earnings: list
+    incertain_earnings: list
+    incertain_expenses: list
+    main_investment: str
+    extra_investments: list
