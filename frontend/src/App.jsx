3799f26e34f7762d60449442d525b642233f147c
Luca Battesini <lucabattesini1@gmail.com>
2025-07-14 15:15:16 -0400

    Setting up a new page in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/index.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./pages/home"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+          <Route path='/' element={<Home />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
