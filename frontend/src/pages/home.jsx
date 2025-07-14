037a18871c5eb863c67e58c83d7d68d37664c74c
Luca Battesini <lucabattesini1@gmail.com>
2025-07-14 15:15:47 -0400

    Creating a Home page

---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,14 @@
+import react from "react";
+
+export default function Home() {
+    return (
+        <div>
+            <h1>
+                Welcome
+            </h1>
+            <p>
+                This is my first react page!
+            </p>
+        </div>
+    )
+}
