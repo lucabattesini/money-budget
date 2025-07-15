ed5c8471eef6c6f20cd80e8a6332f9abb7d9ae5b
Luca Battesini <lucabattesini1@gmail.com>
2025-07-15 14:18:34 -0400

    Creating a function in repo which can create a user profile

--- backend/repositories/create_profile_repo.py
+++ backend/repositories/create_profile_repo.py
@@ -1,1 +1,5 @@
 from db.connection import cursor, connection
+
+def create_profile(name, email, password):
+    cursor.execute("INSERT INTO user_main_profile (name, email, password) VALUES (%s, %s, %s)", (name, email, password))
+    connection.commit()
