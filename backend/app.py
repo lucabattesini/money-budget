5d1ccb3bd14a49e049008657a1425e3b16f348fd
Luca Battesini <lucabattesini1@gmail.com>
2025-07-15 13:38:36 -0400

    Adding permission to the frontend to do requests

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI
-from backend.routers import create_profile_router
+from routers import create_profile_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(create_profile_router.router)
