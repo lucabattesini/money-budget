bf6226625046b0c7fb05c09a14ceff57d6baf808
Luca Battesini <lucabattesini1@gmail.com>
2025-07-15 13:39:58 -0400

    Creating a scripts which sends a json to the back-end

--- frontend/src/pages/create_account.jsx
+++ frontend/src/pages/create_account.jsx
@@ -1,36 +1,70 @@
-import react from "react";
+import React, {useState} from "react";
 
 export default function CreateAccount() {
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault(); // Prevent the autoatic page reload
+
+        const data = {
+            name,
+            email,
+            password,
+        };
+
+        try {
+            const response = await fetch("http://127.0.0.1:8000/create-profile/", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data),
+            });
+
+            const result = await response.json();
+            console.log(result)
+        }   catch (error) {
+                console.error("error")
+            };
+    };
+
     return (
-    <div>
-        <div className="container">
-            <h2>
-                Fill the following inputs
-            </h2>
+        <div>
+            <form onSubmit={handleSubmit}>
+                <div className="container">
+                    <h2>
+                        Fill the following inputs
+                    </h2>
 
-            <label htmlFor="name">Name</label>
-            <input type="text" id="name" />
+                    <label htmlFor="name">Name</label>
+                    <input
+                    id="name"
+                    type="text"
+                    value={name}
+                    onChange={e => setName(e.target.value)} // Atualiza o estado quando digita
+                    />
 
-            <label htmlFor="email">Email</label>
-            <input type="text" id="email" />
+                    <label htmlFor="email">Email</label>
+                    <input 
+                    id="email"
+                    type="text"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                    />
 
-            <label htmlFor="password">Password</label>
-            <input type="text" id="password" />
+                    <label htmlFor="password">Password</label>
+                    <input id="password"
+                    type="text" 
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                    />
+
+                    <button type="submit">Enviar</button>
+                </div>
+            </form>
         </div>
-        <div className="container">
-            <h2>
-                Create your own personalized user profile
-            </h2>
-            
-            <label htmlFor="investment">Main Investment</label>
-            <input type="text" id="name" />
-
-            <label htmlFor="expenses">Fixed spends</label>
-            <input type="text" id="email" />
-
-            <label htmlFor="earnings">Fixed earnings</label>
-            <input type="text" id="password" />
-        </div>
-    </div>
     );
 }
