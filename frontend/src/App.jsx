4d30e6e66b434be8c1ee98c170374de390a88429
Luca Battesini <lucabattesini1@gmail.com>
2025-07-16 16:19:26 -0400

    Importing budget_profile page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import './styles/index.css';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from "./pages/home"
 import CreateAccount from './pages/create_account';
+import BudgetProfile from './pages/create_budget_profile';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
           <Route path='/' element={<Home />}/>
+          <Route path='budget-profile' element={<BudgetProfile/>}/>
           <Route path='create-account' element={<CreateAccount/>}/>
       </Routes>
     </BrowserRouter>
