7c757ffd7e8c44304a95c1a6e4c8ea53e28e5fff
Luca Battesini <lucabattesini1@gmail.com>
2025-07-17 10:34:45 -0400

    Adding reportincome page to app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,9 @@
 import './styles/index.css';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from "./pages/home"
-import CreateAccount from './pages/create_account';
-import BudgetProfile from './pages/create_budget_profile';
+import CreateAccount from './pages/create-account';
+import BudgetProfile from './pages/create-budget-profile';
+import ReportIncome from './pages/report-income';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route path='/' element={<Home />}/>
           <Route path='budget-profile' element={<BudgetProfile/>}/>
           <Route path='create-account' element={<CreateAccount/>}/>
+          <Route path='report-income' element={<ReportIncome/>}/>
       </Routes>
     </BrowserRouter>
   );
