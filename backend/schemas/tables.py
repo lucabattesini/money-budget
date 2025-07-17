9383934d4ad29eec20ac88ea0e1dc2d681df6f92
Luca Battesini <lucabattesini1@gmail.com>
2025-07-17 16:06:13 -0400

    Creating the main tables models to the new migrattions with sqlalchemy

---
+++ backend/schemas/tables.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String, DateTime, Float
+from db.connection import Base
+
+class Transactions(Base):
+    __tablename__ = "transactions"
+
+    id = Column(Integer, primary_key=True)
+    label = Column(String(100))
+    value = Column(Float)
+    date = Column(DateTime)
+    category = Column(String(100))
+
+class Categories(Base):
+    __tablename__ = "categories"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100))
