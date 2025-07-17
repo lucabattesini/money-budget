933b15947802caa546d3003ffd4e174e568af618
Luca Battesini <lucabattesini1@gmail.com>
2025-07-17 11:57:53 -0400

    Deleting an useless schema and creating UserDefaultBudget schema

--- backend/schemas/profile_schema.py
+++ backend/schemas/profile_schema.py
@@ -5,10 +5,7 @@ class UserProfile(BaseModel):
     email: str
     password: str
     
-class UserInvestmentsProfile(BaseModel):
-    fixed_expenses: list
-    fixed_earnings: list
-    incertain_earnings: list
-    incertain_expenses: list
-    main_investment: str
-    extra_investments: list
+class UserDefaultBudget(BaseModel):
+    investments: float
+    incomes: float
+    expenses: float
