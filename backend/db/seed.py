173ff90a81850f2f36ebab24001a0c99bfa6e722
Luca Battesini <lucabattesini1@gmail.com>
2025-07-18 10:54:28 -0400

    Creating a seed which creates the default categories

---
+++ backend/db/seed.py
@@ -0,0 +1,31 @@
+from sqlalchemy.orm import Session
+from db.connection import LocalSession
+from schemas.tables import Categories
+
+default_categories = [
+    "Rent", "Utilities", "Internet & Phone", "Insurance", "Debt Payments", "Taxes", "Super Market",
+    "Food", "Transportation", "Health & Pharmacy", "Personal Care", "Entertainment", "Shopping", "Subscriptions",
+    "Fitness & Sports", "Hobbies", "Travel", "Education", "Home Maintenance", "Pets", "Gifts & Donations",
+    "Special Events", "Emergencies"
+]
+
+def seed_categories():
+    # Create a session with DB
+    db: Session = LocalSession()
+    
+    try:
+        for name in default_categories:
+            # Verify if categorie already exists
+            exist = db.query(Categories).filter_by(name=name).first()
+            if not exist:
+                db.add(Categories(name=name)) # Add the new categorie to DB
+        
+        db.commit() # Commit all changes in DB
+        print("Default categories created successfully")
+    except Exception as e:
+        db.rollback() # If some error happen, it'll undo all the changes
+        print("Error to insert default categories", e)
+    finally:
+        db.close() # Close the session with db
+
+seed_categories()
