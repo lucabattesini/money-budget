1abe516851712055ca12611296cb9abb5996aa8d
Luca Battesini <lucabattesini1@gmail.com>
2025-07-25 16:16:35 -0400

    Adding sqlalchmy to expenses relatorys repo

--- backend/repositories/expenses_repo.py
+++ backend/repositories/expenses_repo.py
@@ -1,4 +1,21 @@
-from db.connection import cursor, connection
+from sqlalchemy.orm import Session
+from db.connection import LocalSession
+from schemas.tables import Transactions
 
-def update_budget_quantity():
-    return
+def insert_transaction(label, value, date, category):
+    db: Session = LocalSession()
+
+    try:
+        db.add(Transactions(
+            label=label,
+            value=value,
+            date=date,
+            category=category
+        ))
+        db.commit()
+        print("Transaction inserted into DB successfully")
+    except Exception as e:
+        db.rollback()
+        print("An error occurred while inserting the transaction")
+    finally:
+        db.close()
