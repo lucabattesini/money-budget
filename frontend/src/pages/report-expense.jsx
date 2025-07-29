c826845ca60436500903248f27a10765dc0e9b23
Luca Battesini <lucabattesini1@gmail.com>
2025-07-29 10:08:27 -0400

    Commiting everything, because I'll move everything to a vite project

--- frontend/src/pages/report-expense.jsx
+++ frontend/src/pages/report-expense.jsx
@@ -2,20 +2,8 @@ import React from "react"
 
 export default function ReportExpense() {
     return (
-        <div className="container">
-            <h1>
-                Report Expenses
-            </h1>
-            <label htmlFor=""></label>
-            <input type="number" />
-
-            <label htmlFor=""></label>
-            <input type="number" />
-
-            <label htmlFor=""></label>
-            <input type="number" />
-
-            <button>Submit</button>
+        <div className="p-6">
+            <button className="btn btn-primary">Submit</button>
         </div>
     );
 }
