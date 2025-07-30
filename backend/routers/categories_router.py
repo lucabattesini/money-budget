96a6b1bb21b44f2cbe7c717a110a4cd8cdc1c23c
Luca Battesini <lucabattesini1@gmail.com>
2025-07-30 10:50:29 -0400

    Creating a route get to get all the categories from db

---
+++ backend/routers/categories_router.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter, status
+from fastapi.responses import JSONResponse
+from schemas.tables import Transactions
+from controllers.categories_controller import organize_categories_ctrl
+
+router = APIRouter(
+    prefix="/categories",
+    tags=["categories"],
+    responses={404: {"description": "Not found"}}
+)
+
+@router.get("/")
+async def get_all_categories_router():
+    categories = organize_categories_ctrl()
+    return JSONResponse(
+        content={"data": categories},
+        status_code=status.HTTP_200_OK
+    )
