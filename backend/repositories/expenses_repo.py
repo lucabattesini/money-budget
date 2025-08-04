214542c01ba9796e07b3d3b80c434064541583e9
Luca Battesini <lucabattesini1@gmail.com>
2025-08-04 12:06:35 -0300

    Creating a function which insert the transaction into the db

--- backend/repositories/expenses_repo.py
+++ backend/repositories/expenses_repo.py
@@ -2,20 +2,15 @@ from sqlalchemy.orm import Session
 from db.connection import LocalSession
 from schemas.tables import Transactions
 
-def insert_transaction(label, value, date, category):
+def report_transaction_repo(id, label, value, date, category):
     db: Session = LocalSession()
 
     try:
-        db.add(Transactions(
-            label=label,
-            value=value,
-            date=date,
-            category=category
-        ))
+        db.add(Transactions(id=id, label=label, value=value, date=date, category=category))
         db.commit()
-        print("Transaction inserted into DB successfully")
     except Exception as e:
         db.rollback()
-        print("An error occurred while inserting the transaction")
     finally:
         db.close()
+    
+    return {"message": "Transaction created successfully"}
