e6491e913771db73cce3cf5174bd79dcfc9bc330
Luca Battesini <lucabattesini1@gmail.com>
2025-08-04 17:30:29 -0300

    Creating a route which will send all the transactions to the front

--- backend/routers/expenses_router.py
+++ backend/routers/expenses_router.py
@@ -1,13 +1,21 @@
 from fastapi import APIRouter, status
 from fastapi.responses import JSONResponse
 from schemas.tables_schemas import Transaction
-from controllers.expenses_controller import report_transaction_ctrl
+from controllers.expenses_controller import report_transaction_ctrl, get_all_transactions_ctrl
 
 router = APIRouter(
     prefix="/report-expense",
     tags=["report-expense"],
     responses={404: {"description": "Not found"}}
 )
+
+@router.get("/")
+async def get_all_transactions_router():
+    transactions = get_all_transactions_ctrl()
+    return JSONResponse(
+        content={"data": transactions},
+        status_code=status.HTTP_200_OK
+    )
 
 @router.post("/")
 async def report_transaction_router(transaction: Transaction):
