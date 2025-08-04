2f88f6aeb9a7c775410ee4d80a822591c794e160
Luca Battesini <lucabattesini1@gmail.com>
2025-08-04 12:24:39 -0300

    Deleting ID from the function request because it'll be create automatically

--- backend/repositories/expenses_repo.py
+++ backend/repositories/expenses_repo.py
@@ -2,11 +2,11 @@ from sqlalchemy.orm import Session
 from db.connection import LocalSession
 from schemas.tables import Transactions
 
-def report_transaction_repo(id, label, value, date, category):
+def report_transaction_repo(label, value, date, category):
     db: Session = LocalSession()
 
     try:
-        db.add(Transactions(id=id, label=label, value=value, date=date, category=category))
+        db.add(Transactions(label=label, value=value, date=date, category=category))
         db.commit()
     except Exception as e:
         db.rollback()
