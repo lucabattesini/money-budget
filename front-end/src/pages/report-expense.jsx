3ea143e885df75448a12c9fbc739e16a87476554
Luca Battesini <lucabattesini1@gmail.com>
2025-08-04 16:10:17 -0300

    Creating a function to send the request from frontend to backend

--- front-end/src/pages/report-expense.jsx
+++ front-end/src/pages/report-expense.jsx
@@ -2,10 +2,11 @@ import { useEffect, useState } from "react";
 import { Center, Stack, Button, Heading, Input, Menu} from "@chakra-ui/react";
 
 export default function ReportExpense() {
-    const [amount, setAmount] = useState();
+    const [value, setvalue] = useState("");
     const [description, setDescription] = useState("");
     const [categories, setCategories] = useState([]);
     const [selectedCategory, setSelectedCategory] = useState("Select category");
+    const [selectCategoryId, setSelectCategoryId] = useState("");
 
     useEffect(() => {
         fetch("http://127.0.0.1:8000/categories/", {
@@ -15,7 +16,37 @@ export default function ReportExpense() {
             .then(data => setCategories(data.data))
 
             .catch(error => console.error("Failed to search categories", error))
-    }, [])
+    }, []);
+
+    const handleSubmit = () => {
+        const payload = {
+            label: description,
+            value: parseInt(value),
+            category: String(selectCategoryId)
+        };
+
+        fetch("http://127.0.0.1:8000/report-expense/", {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(payload)
+        })
+            .then(res => {
+                if (!res.ok) throw new Error("Error to request");
+                return res.json
+            })
+            .then(data => {
+                console.log("Successfully requested", data);
+                
+                setvalue("");
+                setDescription("");
+                setSelectedCategory("Select category");
+            })
+            .catch(err => {
+                console.error("Error to request", err);
+            });
+    };
 
     return (
         <Center>
@@ -30,8 +61,14 @@ export default function ReportExpense() {
                     Report Expense
                 </Heading>
 
-                <Input type="number" placeholder="Amount" variant="outline" />
-                <Input placeholder="Description" variant="outline"/>
+                <Input 
+                type="number" placeholder="Amount" variant="outline" 
+                value={value} onChange={(e) => setvalue(e.target.value)}
+                />
+                <Input
+                placeholder="Description" variant="outline"
+                value={description} onChange={(e) => setDescription(e.target.value)}
+                />
 
                 <Menu.Root>
                     <Menu.Trigger asChild>
@@ -39,14 +76,19 @@ export default function ReportExpense() {
                     </Menu.Trigger>
                     <Menu.Content>
                         {categories?.map((category) => (
-                            <Menu.Item key={category.id} onClick={() => setSelectedCategory(category.name)}>
+                            <Menu.Item key={category.id} 
+                                onClick={() => {
+                                    setSelectedCategory(category.name);
+                                    setSelectCategoryId(category.id);
+                                }}
+                            >
                                 {category.name}
                             </Menu.Item>
                         ))}
                     </Menu.Content>
                 </Menu.Root>
                 
-                <Button>Submit</Button>
+                <Button onClick={handleSubmit}>Submit</Button>
             </Stack>
         </Center>
     );
