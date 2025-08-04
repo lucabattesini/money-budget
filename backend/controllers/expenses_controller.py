40b4d87522549446612aca3b10ee8ec336b1b9dc
Luca Battesini <lucabattesini1@gmail.com>
2025-08-04 17:28:14 -0300

    Creating a function which turn the get transactions objects into json format

--- backend/controllers/expenses_controller.py
+++ backend/controllers/expenses_controller.py
@@ -1,8 +1,15 @@
 from datetime import datetime
-from repositories.expenses_repo import report_transaction_repo
+from repositories.expenses_repo import report_transaction_repo, get_all_transactions_repo
+from fastapi.encoders import jsonable_encoder
 
 def report_transaction_ctrl(label, value, category):
     today = datetime.now()
     report_transaction_repo(label, value, today, category)
     
     return {"message": "Data sent succesfully"}
+
+def get_all_transactions_ctrl():
+    transactions = get_all_transactions_repo()
+    json_result = jsonable_encoder(transactions)
+    
+    return json_result
