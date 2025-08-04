d5d8dc4331c332540bc36874061678ca37fe23ad
Luca Battesini <lucabattesini1@gmail.com>
2025-08-04 17:31:15 -0300

    Creating a function that request the transactions from backend and convert it into cards

---
+++ front-end/src/pages/transactions-display.jsx
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react";
+import { Center, Stack, Heading, Card} from "@chakra-ui/react";
+
+export default function TransactionsDisplay() {
+    const [transactions, setTransactions] = useState([]);
+
+    useEffect(() => {
+        fetch("http://127.0.0.1:8000/report-expense/", {
+            method: "get"
+        })
+            .then(Response => Response.json())
+            .then(data => setTransactions(data.data))
+
+            .catch(error => console.error("Failed to get transactions", error))
+    }, []);
+
+    return(
+        <Center>
+            <Stack
+                height="100vh"
+                gap={6}
+                width="35vh"
+                align="center"
+                justify="center"
+            >
+                <Heading>
+                    Transactions list
+                </Heading>
+
+                {transactions?.map((item) => (
+                    <Card.Root size="sm">
+                      <Card.Header>
+                        <Heading size="md"> {item.value} - {item.category}</Heading>
+                      </Card.Header>
+                      <Card.Body color="fg.muted">
+                        {item.label}
+                      </Card.Body>
+                    </Card.Root>
+                ))}
+
+            </Stack>
+        </Center>
+    )
+}
