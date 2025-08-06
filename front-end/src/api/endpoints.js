e12b9ab195b689f1cba1ecea37e082f6ee2861db
Luca Battesini <lucabattesini1@gmail.com>
2025-08-06 10:11:27 -0300

    Creating a file with the URL addresses

---
+++ front-end/src/api/endpoints.js
@@ -0,0 +1,9 @@
+
+const URL_BASE = "http://127.0.0.1:8000";
+
+const endpoints ={
+    categories: `${URL_BASE}/categories`,
+    expenses: `${URL_BASE}/report-expense`
+};
+
+export default endpoints;
