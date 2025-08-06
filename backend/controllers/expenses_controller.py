c5d56a4e63854d208288be2963236993cabee758
Luca Battesini <lucabattesini1@gmail.com>
2025-08-06 09:58:46 -0300

    Returning a status code instead of a message in get expenses_controller

--- backend/controllers/expenses_controller.py
+++ backend/controllers/expenses_controller.py
@@ -1,12 +1,13 @@
 from datetime import datetime
 from repositories.expenses_repo import report_transaction_repo, get_all_transactions_repo
 from fastapi.encoders import jsonable_encoder
+from fastapi import status
 
 def report_transaction_ctrl(label, value, category):
     today = datetime.now()
     report_transaction_repo(label, value, today, category)
     
-    return {"message": "Data sent succesfully"}
+    return status.HTTP_202_ACCEPTED
 
 def get_all_transactions_ctrl():
     transactions = get_all_transactions_repo()
