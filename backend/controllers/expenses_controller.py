0c186c73a474a2b52b3841b0d5c533dbcfb0c1bb
Luca Battesini <lucabattesini1@gmail.com>
2025-08-12 16:24:24 -0300

    Creating a function which get transactions by a limit date and returning it by an endpoint

--- backend/controllers/expenses_controller.py
+++ backend/controllers/expenses_controller.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from repositories.expenses_repo import report_transaction_repo, get_all_transactions_repo
 from fastapi import status
 
@@ -13,3 +13,14 @@ def get_all_transactions_ctrl():
     transactions = get_all_transactions_repo()
     
     return transactions
+
+def get_transactions_by_time_ctrl(time):
+    time = int(time)
+    transactions = get_all_transactions_repo()
+    today = datetime.now()
+    filtered_transactions = [
+        r for r in transactions
+        if (today - r.date).days <= time
+    ]
+
+    return filtered_transactions
--- backend/routers/expenses_router.py
+++ backend/routers/expenses_router.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, status
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 from schemas.tables_schemas import Transaction
-from controllers.expenses_controller import report_transaction_ctrl, get_all_transactions_ctrl
+from controllers.expenses_controller import report_transaction_ctrl, get_all_transactions_ctrl, get_transactions_by_time_ctrl
 
 router = APIRouter(
     prefix="/report-expense",
@@ -14,6 +14,15 @@ router = APIRouter(
 async def get_all_transactions_router():
     transactions = get_all_transactions_ctrl()
     json_result = jsonable_encoder(transactions)
+    return JSONResponse(
+        content={"data": json_result},
+        status_code=status.HTTP_200_OK
+    )
+
+@router.get("/time-filtered/{time}")
+async def get_transactions_by_time(time):
+    filtered_transactions = get_transactions_by_time_ctrl(time)
+    json_result = jsonable_encoder(filtered_transactions)
     return JSONResponse(
         content={"data": json_result},
         status_code=status.HTTP_200_OK
