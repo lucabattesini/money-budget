81be09a82fe8ed930b3618e72c8a167d6148616b
Luca Battesini <lucabattesini1@gmail.com>
2025-08-15 15:44:56 -0300

    Creating a line chart based on categories and its expenses values

--- front-end/src/pages/dashboard.jsx
+++ front-end/src/pages/dashboard.jsx
@@ -1,18 +1,43 @@
+"use client"
+
+import { BarList, useChart} from "@chakra-ui/charts";
 import { useState, useEffect } from "react"
-import { Stack, Center, Heading } from "@chakra-ui/react"
-import { getAllCategories, getAllTransactions } from "../api/endpoints";
+import { Stack, Center, Heading, Box } from "@chakra-ui/react"
+import { getAllCategories, getAddedTransactionsByCategory } from "../api/endpoints";
 
 export default function Dashboard() {
-    const [transactions, setTransactions] = useState([]);
+    const [categoriesAdded, setCategoriesAdded] = useState([]);
     const [categories, setCategories] = useState([]);
+    const organizedCategories = []
     useEffect(() => {
             getAllCategories().then(data => setCategories(data.data))
         }, []);
 
     useEffect(() => {
-            getAllTransactions().then(data => setTransactions(data.data))
+        getAddedTransactionsByCategory().then(data => setCategoriesAdded(data.data))
         }, []);
-    
+
+    for (const r of categoriesAdded) {
+        const categorieName = categories.find(element => element.id == r.category)
+
+        if (!categorieName) continue;
+        const organizedCategory = {
+            name: categorieName.name,
+            value: r.total / 100
+        };
+
+        organizedCategories.push(organizedCategory);
+
+    }
+
+    const chart = useChart({
+        sort: { by: "value", direction: "desc"},
+        data: organizedCategories.map((item) => ({
+            name: `${item.name} `,
+            value: item.value
+        })),
+        series: [{name: "name", color: "teal.subtle"}],
+    });
     return(
         <Center>
             <Stack
@@ -26,6 +51,14 @@ export default function Dashboard() {
                 <Heading>
                     Dashboard
                 </Heading>
+                <Box w="100%">
+                    <BarList.Root chart={chart}>
+                        <BarList.Content>
+                            <BarList.Bar/>
+                            <BarList.Value/>
+                        </BarList.Content>
+                    </BarList.Root>
+                </Box>
             </Stack>
         </Center>
     )
