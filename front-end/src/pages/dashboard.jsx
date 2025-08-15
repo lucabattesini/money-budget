7b76feea24cddd43ac008ac4bc5d77143c2b7818
Luca Battesini <lucabattesini1@gmail.com>
2025-08-15 16:03:31 -0300

    Deleting the function from dashboard.jsx

--- front-end/src/pages/dashboard.jsx
+++ front-end/src/pages/dashboard.jsx
@@ -1,43 +1,8 @@
-"use client"
-
-import { BarList, useChart} from "@chakra-ui/charts";
 import { useState, useEffect } from "react"
-import { Stack, Center, Heading, Box } from "@chakra-ui/react"
-import { getAllCategories, getAddedTransactionsByCategory } from "../api/endpoints";
+import { Stack, Center, Heading } from "@chakra-ui/react"
+import BarListChart from "../utils/charts"
 
 export default function Dashboard() {
-    const [categoriesAdded, setCategoriesAdded] = useState([]);
-    const [categories, setCategories] = useState([]);
-    const organizedCategories = []
-    useEffect(() => {
-            getAllCategories().then(data => setCategories(data.data))
-        }, []);
-
-    useEffect(() => {
-        getAddedTransactionsByCategory().then(data => setCategoriesAdded(data.data))
-        }, []);
-
-    for (const r of categoriesAdded) {
-        const categorieName = categories.find(element => element.id == r.category)
-
-        if (!categorieName) continue;
-        const organizedCategory = {
-            name: categorieName.name,
-            value: r.total / 100
-        };
-
-        organizedCategories.push(organizedCategory);
-
-    }
-
-    const chart = useChart({
-        sort: { by: "value", direction: "desc"},
-        data: organizedCategories.map((item) => ({
-            name: `${item.name} `,
-            value: item.value
-        })),
-        series: [{name: "name", color: "teal.subtle"}],
-    });
     return(
         <Center>
             <Stack
@@ -51,14 +16,7 @@ export default function Dashboard() {
                 <Heading>
                     Dashboard
                 </Heading>
-                <Box w="100%">
-                    <BarList.Root chart={chart}>
-                        <BarList.Content>
-                            <BarList.Bar/>
-                            <BarList.Value/>
-                        </BarList.Content>
-                    </BarList.Root>
-                </Box>
+                <BarListChart/>
             </Stack>
         </Center>
     )
