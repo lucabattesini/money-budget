144f68624412e702623986fe50ddac216c908ece
Luca Battesini <lucabattesini1@gmail.com>
2025-08-18 12:44:20 -0300

    Creating a Menu page to the aplication where you can flow betwen the pages

--- front-end/src/pages/home.jsx
+++ front-end/src/pages/home.jsx
@@ -1,14 +1,19 @@
-import { Heading, Text } from "@chakra-ui/react";
+import { Heading, Text, Tabs } from "@chakra-ui/react";
+import Dashboard from "./dashboard";
+import ReportExpense from "./report-expense";
+import TransactionsDisplay from "./transactions-display";
 
 export default function Home() {
     return (
-        <div>
-            <Heading>
-                Welcome page
-            </Heading>
-            <Text>
-                Main page
-            </Text>
-        </div>
+      <Tabs.Root>
+        <Tabs.List>
+            <Tabs.Trigger value="dashboard">Dashboard</Tabs.Trigger>
+            <Tabs.Trigger value="report-expense">Report Expense</Tabs.Trigger>
+            <Tabs.Trigger value="transactions-list">Transactions List</Tabs.Trigger>
+        </Tabs.List>
+        <Tabs.Content value="dashboard"> <Dashboard/></Tabs.Content>
+        <Tabs.Content value="report-expense"> <ReportExpense/> </Tabs.Content>
+        <Tabs.Content value="transactions-list"> <TransactionsDisplay/> </Tabs.Content>
+      </Tabs.Root>  
     );
 }
