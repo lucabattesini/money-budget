3ab82988a569505acc47af0bd223308f43246409
Luca Battesini <lucabattesini1@gmail.com>
2025-08-19 17:09:48 -0300

    Creating the structure of users table and adapting the structure of others

--- back-end/schemas/tables.py
+++ back-end/schemas/tables.py
@@ -5,13 +5,26 @@ class Transactions(Base):
     __tablename__ = "transactions"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(String(100))
     label = Column(String(100))
     value = Column(Float)
     date = Column(DateTime)
     category = Column(String(100))
 
+class Users(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(100))
+    email = Column(String(100))
+    password = Column(String(100))
+    created_at = Column(DateTime)
+    updated_at = Column(DateTime)
+    last_login = Column(DateTime)
+
 class Categories(Base):
     __tablename__ = "categories"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(String(100))
     name = Column(String(100))
