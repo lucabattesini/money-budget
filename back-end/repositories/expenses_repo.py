e0bcb20b81a9261a3da6ff55c8505fe863f0afe5
Luca Battesini <lucabattesini1@gmail.com>
2025-08-19 10:48:32 -0300

    Adding a date filter for the dashboard get function, it'll just return theis month transactions

--- back-end/repositories/expenses_repo.py
+++ back-end/repositories/expenses_repo.py
@@ -1,11 +1,11 @@
 from sqlalchemy.orm import Session
-from sqlalchemy import func
+from sqlalchemy import func, extract
 from db.connection import LocalSession
 from schemas.tables import Transactions
+from datetime import datetime
 
 def report_transaction_repo(label, value, date, category):
     db: Session = LocalSession()
-
     try:
         db.add(Transactions(label=label, value=value, date=date, category=category))
         db.commit()
@@ -18,17 +18,29 @@ def report_transaction_repo(label, value, date, category):
 
 def get_all_transactions_repo():
     db : Session = LocalSession()
-
     transactions = db.query(Transactions).all()
 
     return transactions
- 
+
 def get_added_transactions_by_category_repo():
+    now = datetime.now()
+    current_month = now.strftime("%m")
+    current_year = now.strftime("%Y")
+
     db : Session = LocalSession()
     added_categoires = (
-        db.query(Transactions.category, func.sum(Transactions.value)).group_by(Transactions.category).all()
+        db.query(
+            Transactions.category,
+            func.sum(Transactions.value))
+            .filter(extract("month", Transactions.date) == current_month)
+            .filter(extract("year", Transactions.date) == current_year)
+            .group_by(Transactions.category)
+            .all()
     )
+
     return [
         {"category": category, "total": total}
         for category, total in added_categoires
     ]
+
+get_added_transactions_by_category_repo()
