484be1fa2e09dc907c16eaf0d4aa458538df990a
Luca Battesini <lucabattesini1@gmail.com>
2025-08-19 17:10:12 -0300

    Adding login and create account pages to app

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,12 +1,16 @@
 import './styles/index.css';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from "./pages/home"
+import Login from './pages/login';
+import CreateAccount from './pages/create-account';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-          <Route path='/' element={<Home />}/>
+          <Route path='/' element={ <Home/> }/>
+          <Route path='/login' element={ <Login/> }/>
+          <Route path='/create-account' element={ <CreateAccount/> }/>
       </Routes>
     </BrowserRouter>
   );
