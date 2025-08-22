0b2cd04fee485bfe6e59222425ed9b8d2b342b98
Luca Battesini <lucabattesini1@gmail.com>
2025-08-22 17:25:43 -0300

    Creating user login function in ctrl

--- back-end/controllers/users_controller.py
+++ back-end/controllers/users_controller.py
@@ -1,7 +1,11 @@
 from datetime import datetime
-from repositories.users_repo import create_user
+from repositories.users_repo import create_user_repo, user_login_repo
 
 def create_user_ctrl(name, email, password):
     today = datetime.now()
-    created_user = create_user(name, email, password, today, today, None, False)
+    created_user = create_user_repo(name, email, password, today, today, None, False)
     return created_user
+
+def user_login_ctrl(email, password):
+    logged_user = user_login_repo(email, password)
+    return logged_user
