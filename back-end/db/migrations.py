ebfad54859902296ef9c62524134270fa687505a
Luca Battesini <lucabattesini1@gmail.com>
2025-08-25 12:49:58 -0300

    Rewriting the paths in correct way

--- back-end/db/migrations.py
+++ back-end/db/migrations.py
@@ -1,4 +1,4 @@
-from connection import Base, engine
-from ..schemas.tables import Transactions, Categories, Users
+from .connection import Base, engine
+from schemas.tables import Transactions, Categories, Users
 
 Base.metadata.create_all(bind=engine)
