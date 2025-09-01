5429a21614f3a05ae3a629b7c363d7218cc19cb8
Luca Battesini <lucabattesini1@gmail.com>
2025-09-01 13:04:15 -0300

    Changing some tables in tables_schemas

--- back-end/schemas/tables_schemas.py
+++ back-end/schemas/tables_schemas.py
@@ -1,15 +1,14 @@
 from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime
 
 class Transaction(BaseModel):
     label: str
     value: int
     category: str
-
+    date: Optional[datetime] = None
+ 
 class User(BaseModel):
-    name: str
+    name: Optional[str] = None
     email: str 
-    password: str
-
-class UserLogin(BaseModel):
-    email: str
     password: str
