ef6f0ea57b44062dacf7f64a8f91c816968c3093
Luca Battesini <lucabattesini1@gmail.com>
2025-09-12 15:01:06 -0300

    Returning transaction list from newest to oldest

--- back-end/controllers/transactions_controller.py
+++ back-end/controllers/transactions_controller.py
@@ -9,7 +9,12 @@ def create_transaction(transaction: Transaction):
     return object
 
 def get_all_transactions():
-    return get_all_transactions_repo()
+    all_transactions = get_all_transactions_repo()
+    increasing_ordered_list = sorted(all_transactions, key=lambda e: e.date)
+    decreasing_ordered_list = []
+    for r in increasing_ordered_list:
+        decreasing_ordered_list.insert(0, r)
+    return decreasing_ordered_list
 
 def get_transactions_summed_by_category():
     today = datetime.now()
